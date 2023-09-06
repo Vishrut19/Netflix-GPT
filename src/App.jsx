935d6304631145fb95ed7e25db1e1f1a3c3f87e9
Vishrut19 <vishrutagarwalla19@gmail.com>
2023-09-07 00:03:30 +0530

    vite & tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="font-bold text-3xl text-green-500">
+        Hello! Let&apos;s Build Netflix GPT
+      </p>
+    </>
+  );
+}
+
+export default App;
