9a963da7acb856b8b2d9ac2fa252f7a52b3199d4
Vishrut19 <vishrutagarwalla19@gmail.com>
2023-09-13 16:06:44 +0530

    Video Background Added Like Netflix Landing Page

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -7,7 +7,12 @@ const VideoBackground = ({ movieId }) => {
   return (
     <div>
       <iframe
-        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerVideo?.key +
+          "?&autoplay=1&mute=1"
+        }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/prop-types */
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="px-12 pt-36">
+    <div className="absolute w-screen px-24 text-white aspect-video pt-[20%] bg-gradient-to-r from-black">
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="w-1/4 py-6 text-lg">{overview}</p>
       <div>
-        <button className="p-4 px-12 text-xl text-white bg-gray-500 bg-opacity-50 rounded-lg">
+        <button className="p-4 px-12 text-xl text-black bg-white rounded-lg hover:bg-opacity-80">
           ▶️ Play
         </button>
-        <button className="p-4 px-12 mx-2 text-xl text-white bg-gray-500 bg-opacity-50 rounded-lg">
+        <button className="p-4 px-12 mx-2 text-xl bg-gray-500 bg-opacity-50 rounded-lg hover:bg-opacity-20">
           &#9432; More Info
         </button>
       </div>
