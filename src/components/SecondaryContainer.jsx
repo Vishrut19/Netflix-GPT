d388070b10ebdeae96160e8990e0a5a11cf6180f
Vishrut19 <vishrutagarwalla19@gmail.com>
2023-09-17 11:43:54 +0530

    Refactored List's Title

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -8,10 +8,9 @@ const SecondaryContainer = () => {
       <div className=" bg-black">
         <div className="-mt-52 relative z-20">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-          <MovieList title={"Trending"} movies={movies.topRatedMovies} />
+          <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
           <MovieList title={"Popular Movies "} movies={movies.popularMovies} />
           <MovieList title={"Upcoming Movies"} movies={movies.upcomingMovies} />
-          <MovieList title={"Action"} movies={movies.nowPlayingMovies} />
         </div>
       </div>
     )
